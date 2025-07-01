214eced34140d81bb963eefc06595c1da2decf10
Md Mubin <fontdevmubin@gmail.com>
2025-07-01 22:02:47 +0600

    worked on role based middleware

--- server/router/api/product.js
+++ server/router/api/product.js
@@ -2,9 +2,10 @@ const express = require("express")
 const { createCetegory, fetchCetegories } = require("../../controller/cetegoryController")
 const authMiddleware = require("../../middlewares/authMiddleware")
 const chekingRoles = require("../../middlewares/cetegoryMiddleware")
+const upload = require("../../helpers/multer")
 const productRoute = express.Router()
 
-productRoute.post("/create_category",authMiddleware, chekingRoles(["admin"]), createCetegory)
-productRoute.fet("/allCetegories",authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
+productRoute.post("/create_category",authMiddleware, chekingRoles(["admin"]), upload.single("Cetegory"), createCetegory)
+productRoute.get("/allCetegories",authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
 
 module.exports = productRoute
