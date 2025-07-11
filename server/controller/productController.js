fb3573f4fdbfd4c96d09c465795ca4566bcf28c1
Md Mubin <fontdevmubin@gmail.com>
2025-07-11 23:49:10 +0600

    modified:server/controller/productController.js added req res

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -2,10 +2,18 @@
 // ================== create product
 const create_product = (req, res) => {
 
+    const { title, description, price, stock, mainImg, cetegory, veriants } = req.body
+
+    if (!title) return res.status(400).send({ err: "Product Title Required" })
+    if (!description) return res.status(400).send({ err: "Product Description Required" })
+    if (!price) return res.status(400).send({ err: "Product Price Required" })
+    if (!stock) return res.status(400).send({ err: "Product Stock Required" })
+    if (!mainImg) return res.status(400).send({ err: "Product Main Image Required" })
+    if (!cetegory) return res.status(400).send({ err: "Product Cetegory Required" })
 }
 
 // ================== fetching all product
-const fetch_allProduct =(req,res)=>{
+const fetch_allProduct = (req, res) => {
 
 }
 
