4a42832a5847fd48e3cd9253234a7a6371853338
Md Mubin <fontdevmubin@gmail.com>
2025-07-11 23:51:11 +0600

    modified:server/controller/productController.js added schema save

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,15 +1,27 @@
+const productSchema = require("../models/productSchema")
 
 // ================== create product
 const create_product = (req, res) => {
 
-    const { title, description, price, stock, mainImg, cetegory, veriants } = req.body
-
-    if (!title) return res.status(400).send({ err: "Product Title Required" })
-    if (!description) return res.status(400).send({ err: "Product Description Required" })
-    if (!price) return res.status(400).send({ err: "Product Price Required" })
-    if (!stock) return res.status(400).send({ err: "Product Stock Required" })
-    if (!mainImg) return res.status(400).send({ err: "Product Main Image Required" })
-    if (!cetegory) return res.status(400).send({ err: "Product Cetegory Required" })
+    try {
+        const { title, description, price, stock, mainImg, cetegory, veriants } = req.body
+    
+        if (!title) return res.status(400).send({ err: "Product Title Required" })
+        if (!description) return res.status(400).send({ err: "Product Description Required" })
+        if (!price) return res.status(400).send({ err: "Product Price Required" })
+        if (!stock) return res.status(400).send({ err: "Product Stock Required" })
+        if (!mainImg) return res.status(400).send({ err: "Product Main Image Required" })
+        if (!cetegory) return res.status(400).send({ err: "Product Cetegory Required" })
+    
+        const newProduct = new productSchema({
+            title, description, price, stock, mainImg, cetegory, veriants
+        })
+    
+        newProduct.save()
+        res.status(200).send({ msg: "New Product Created Successfully" })
+    } catch (error) {
+        res.status(500).send({ err: "Server Error" })
+    }
 }
 
 // ================== fetching all product
