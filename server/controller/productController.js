5b4a5db8de80404047fe8ef70d473dc9871e5b4b
Md Mubin <fontdevmubin@gmail.com>
2025-07-11 23:38:30 +0600

    new file: server/controller/productController.js 	modified: server/router/api/product.js

---
+++ server/controller/productController.js
@@ -0,0 +1,12 @@
+
+// ================== create product
+const create_product = (req, res) => {
+
+}
+
+// ================== fetching all product
+const fetch_allProduct =(req,res)=>{
+
+}
+
+module.exports = { create_product, fetch_allProduct }
--- server/router/api/product.js
+++ server/router/api/product.js
@@ -3,9 +3,13 @@ const { createCetegory, fetchCetegories } = require("../../controller/cetegoryCo
 const authMiddleware = require("../../middlewares/authMiddleware")
 const chekingRoles = require("../../middlewares/cetegoryMiddleware")
 const upload = require("../../helpers/multer")
+const { create_product, fetch_allProduct } = require("../../controller/productController")
 const productRoute = express.Router()
 
 productRoute.post("/create_category",authMiddleware, chekingRoles(["admin"]), upload.single("Cetegory"), createCetegory)
 productRoute.get("/allCetegories",authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
+
+productRoute.post("/create_product", create_product)
+productRoute.post("/allProduct", fetch_allProduct)
 
 module.exports = productRoute
