4d19fdb1d38c1d2f235509ed1d4e56e49b8a7916
Md Mubin <fontdevmubin@gmail.com>
2025-07-15 23:24:41 +0600

    fixed created and update product router

--- server/router/api/product.js
+++ server/router/api/product.js
@@ -3,13 +3,14 @@ const { createCetegory, fetchCetegories } = require("../../controller/cetegoryCo
 const authMiddleware = require("../../middlewares/authMiddleware")
 const chekingRoles = require("../../middlewares/cetegoryMiddleware")
 const upload = require("../../helpers/multer")
-const { create_product, fetch_allProduct } = require("../../controller/productController")
+const { create_product, fetch_allProduct, update_product } = require("../../controller/productController")
 const productRoute = express.Router()
 
-productRoute.post("/create_category",authMiddleware, chekingRoles(["admin"]), upload.single("Cetegory"), createCetegory)
-productRoute.get("/allCetegories",authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
+productRoute.post("/create_category", authMiddleware, chekingRoles(["admin"]), upload.single("Cetegory"), createCetegory)
+productRoute.get("/allCetegories", authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
 
-productRoute.post("/create_product", create_product)
+productRoute.post("/create_product", upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]), create_product)
+productRoute.post("/update_product/:slug", upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]), update_product)
 productRoute.get("/allProduct", fetch_allProduct)
 
 module.exports = productRoute
