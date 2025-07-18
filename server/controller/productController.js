85afdb920f1a70ca64b31d520b4b31bb0f51c77b
Md Mubin <fontdevmubin@gmail.com>
2025-07-19 00:10:20 +0600

    fixed somehting

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -8,84 +8,84 @@ const cetegorySchema = require("../models/cetegorySchema")
 // ================== create product
 const create_product = async (req, res) => {
 
-    // try {
-    const { title, description, price, stock, cetegory, veriants } = req.body
+    try {
+        const { title, description, price, stock, cetegory, veriants } = req.body
 
-    if (!title) return res.status(400).send({ err: "Product Title Required" })
-    if (!description) return res.status(400).send({ err: "Product Description Required" })
-    if (!price) return res.status(400).send({ err: "Product Price Required" })
-    if (!stock) return res.status(400).send({ err: "Product Stock Required" })
-    // if (veriants.length < 1) return res.status(400).send({ err: "Product Veriants Required" })
+        if (!title) return res.status(400).send({ err: "Product Title Required" })
+        if (!description) return res.status(400).send({ err: "Product Description Required" })
+        if (!price) return res.status(400).send({ err: "Product Price Required" })
+        if (!stock) return res.status(400).send({ err: "Product Stock Required" })
+        // if (veriants.length < 1) return res.status(400).send({ err: "Product Veriants Required" })
 
-    // for chekcing image
-    if (!req?.files?.mainImg) return res.status(400).send({ err: "Product Main Image Required" })
+        // for chekcing image
+        if (!req?.files?.mainImg) return res.status(400).send({ err: "Product Main Image Required" })
 
-    // generate slugs
-    const generatedSlug = await slugGenerator(title)
+        // generate slugs
+        const generatedSlug = await slugGenerator(title)
 
-    // check if the product exists
-    const existsProduct = await productSchema.findOne({ slug: generatedSlug })
-    if (existsProduct) return res.status(400).send({ err: "Product Already Exists" })
+        // check if the product exists
+        const existsProduct = await productSchema.findOne({ slug: generatedSlug })
+        if (existsProduct) return res.status(400).send({ err: "Product Already Exists" })
 
-    // upload main image
-    let productMainImg
-    if (req?.files?.mainImg.length > 0) {
-        for (item of req?.files?.mainImg) {
-            const result = await cloudinary.uploader.upload(item?.path, {
-                folder: "product"
-            })
-            
-            fs.unlinkSync(item?.path)
-            productMainImg = result.url
+        // upload main image
+        let productMainImg
+        if (req?.files?.mainImg.length > 0) {
+            for (item of req?.files?.mainImg) {
+                const result = await cloudinary.uploader.upload(item?.path, {
+                    folder: "product"
+                })
+
+                fs.unlinkSync(item?.path)
+                productMainImg = result.url
+            }
         }
-    }
 
-    // upload sub images
-    let productSubImages = []
-    if (req?.files?.subImgs.length > 0) {
-        for (item of req?.files?.subImgs) {
-            const result = await cloudinary.uploader.upload(item?.path, {
-                folder: "product"
-            })
-            fs.unlinkSync(item?.path)
-            productSubImages.push(result.url)
+        // upload sub images
+        let productSubImages = []
+        if (req?.files?.subImgs.length > 0) {
+            for (item of req?.files?.subImgs) {
+                const result = await cloudinary.uploader.upload(item?.path, {
+                    folder: "product"
+                })
+                fs.unlinkSync(item?.path)
+                productSubImages.push(result.url)
+            }
         }
+
+        if (!cetegory) return res.status(400).send({ err: "Product Cetegory Required" })
+
+        // for veriants conditions
+        veriants.forEach((items) => {
+            if (!["color", "size"].includes(items.name)) {
+                return res.status(400).send({ err: "Invalid Veriants Options" })
+            }
+
+            if (items.name === "color") {
+                items.options.forEach(colorOption => {
+                    if (!colorOption.hasOwnProperty("colorName")) {
+                        return res.status(400).send({ err: "Veriant's Color Name is Required" })
+                    }
+                })
+            }
+
+            if (items.name === "size") {
+                items?.options.forEach(sizeOption => {
+                    if (!sizeOption.hasOwnProperty("allSizes")) {
+                        return res.status(400).send({ err: "Veriant's Size is Required" })
+                    }
+                })
+            }
+        })
+
+        const newProduct = new productSchema({
+            title, slug: generatedSlug, description, price, stock, mainImg: productMainImg, subImgs: productSubImages, cetegory, veriants
+        })
+
+        newProduct.save()
+        res.status(200).send({ msg: "New Product Created Successfully" })
+    } catch (error) {
+        res.status(500).send({ err: "Server Error" })
     }
-
-    if (!cetegory) return res.status(400).send({ err: "Product Cetegory Required" })
-
-    // for veriants conditions
-    // veriants.forEach((items) => {
-    //     if (!["color", "size"].includes(items.name)) {
-    //         return res.status(400).send({ err: "Invalid Veriants Options" })
-    //     }
-
-    //     if (items.name === "color") {
-    //         items.options.forEach(colorOption => {
-    //             if (!colorOption.hasOwnProperty("colorName")) {
-    //                 return res.status(400).send({ err: "Veriant's Color Name is Required" })
-    //             }
-    //         })
-    //     }
-
-    //     if (items.name === "size") {
-    //         items?.options.forEach(sizeOption => {
-    //             if (!sizeOption.hasOwnProperty("allSizes")) {
-    //                 return res.status(400).send({ err: "Veriant's Size is Required" })
-    //             }
-    //         })
-    //     }
-    // })
-
-    const newProduct = new productSchema({
-        title, slug: generatedSlug, description, price, stock, mainImg: productMainImg, subImgs: productSubImages, cetegory, veriants
-    })
-
-    newProduct.save()
-    res.status(200).send({ msg: "New Product Created Successfully" })
-    // } catch (error) {
-    //     res.status(500).send({ err: "Server Error" })
-    // }
 }
 
 // ================== updating product
