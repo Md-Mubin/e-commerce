19f9e85df0d9224b54dd2903784fd342a98e3283
Md Mubin <fontdevmubin@gmail.com>
2025-07-19 00:13:01 +0600

    ___

--- server/router/api/product.js
+++ server/router/api/product.js
@@ -6,12 +6,39 @@ const upload = require("../../helpers/multer")
 const { create_product, fetch_allProduct, update_product, delete_product } = require("../../controller/productController")
 const productRoute = express.Router()
 
-productRoute.post("/create_category", authMiddleware, chekingRoles(["admin"]), upload.single("Cetegory"), createCetegory)
-productRoute.get("/allCetegories", authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetchCetegories)
+// ============= cetegory part 
+productRoute.post("/create_category",
+    authMiddleware,
+    chekingRoles(["admin"]),
+    upload.single("Cetegory"),
+    createCetegory)
 
-productRoute.post("/create_product", authMiddleware, chekingRoles(["admin", "stuffs"]), upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]), create_product)
-productRoute.post("/update_product/:slug", authMiddleware, chekingRoles(["admin", "stuffs"]), upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]), update_product)
-productRoute.get("/allProduct", authMiddleware, chekingRoles(["users", "admin", "stuffs"]), fetch_allProduct)
-productRoute.delete("/deleteProduct/:productID", authMiddleware, chekingRoles(["admin"]), delete_product)
+productRoute.get("/allCetegories",
+    authMiddleware,
+    chekingRoles(["users", "admin", "stuffs"]),
+    fetchCetegories)
+
+// ============= product part 
+productRoute.post("/create_product",
+    authMiddleware,
+    chekingRoles(["admin", "stuffs"]),
+    upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]),
+    create_product)
+
+productRoute.post("/update_product/:slug",
+    authMiddleware,
+    chekingRoles(["admin", "stuffs"]),
+    upload.fields([{ name: "mainImg", maxCount: 1 }, { name: "subImgs", maxCount: 6 }]),
+    update_product)
+
+productRoute.get("/allProduct",
+    authMiddleware,
+    chekingRoles(["users", "admin", "stuffs"]),
+    fetch_allProduct)
+
+productRoute.delete("/deleteProduct/:productID",
+    authMiddleware,
+    chekingRoles(["admin"]),
+    delete_product)
 
 module.exports = productRoute
