55d08aae25c66e2e68d1c183c2d211d9af06d5aa
Md Mubin <fontdevmubin@gmail.com>
2025-07-19 11:52:08 +0600

    __

--- server/router/api/auth.js
+++ server/router/api/auth.js
@@ -5,11 +5,18 @@ const upload = require("../../helpers/multer")
 const chekingRoles = require("../../middlewares/cetegoryMiddleware")
 const authRoute = express.Router()
 
-authRoute.post("/register", register)
-authRoute.post("/login", login)
-authRoute.post("/emailVerify", emailVarify)
-authRoute.post("/resetPass/:randomString", resetPass)
-authRoute.post("/forgetPass", forgetPass)
-authRoute.post("/update", authMiddleware, chekingRoles(["users", "admin", "stuffs"]), upload.single("avatar"), update)
+authRoute.post("/register", register) // register route
+authRoute.post("/emailVerify", emailVarify) // email verify route
+authRoute.post("/login", login) // login route
+
+authRoute.post("/resetPass/:randomString", resetPass) // reset password route
+authRoute.post("/forgetPass", forgetPass) // forget password route
+
+// updating profile route
+authRoute.post("/update",
+    authMiddleware,
+    chekingRoles(["users", "admin", "stuffs"]),
+    upload.single("avatar"),
+    update)
 
 module.exports = authRoute
