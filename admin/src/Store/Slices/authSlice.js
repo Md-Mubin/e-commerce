d6932d5a7877a5c463bab00302529bac231e78ff
Md Mubin <fontdevmubin@gmail.com>
2025-08-11 23:48:04 +0600

    setUp redux and slice and store

---
+++ admin/src/Store/Slices/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: JSON.parse(localStorage.getItem("loggedUser")) || null,
+  },
+  reducers: {
+    loggedUserInfo: (state, action) => {
+      state.user = action.payload
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { loggedUserInfo } = authSlice.actions
+
+export default counterSlice.reducer
