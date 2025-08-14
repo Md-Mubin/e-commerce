eaeff74844593f13b6f405709a4f9d42244dc41c
Md Mubin <fontdevmubin@gmail.com>
2025-08-14 23:10:26 +0600

    _

--- server/helpers/emailPassValid.js
+++ server/helpers/emailPassValid.js
@@ -11,19 +11,19 @@ const emailValid = (userEmail) => {
 const passValid = (userPass) => {
 
     if (userPass.length < 6 || userPass.length > 10) {
-        return { error: "Password must be 6-10 charecter" }
+        return "Password must be 6-10 charecter"
     }
 
     if (!/[A-Z]/.test(userPass)) {
-        return { error: "Password must have a capital letter" }
+        return "Password must have a capital letter"
     }
 
     if (!/[a-z]/.test(userPass)) {
-        return { error: "Password must have a small letter" }
+        return "Password must have a small letter"
     }
 
     if (!/[0-9]/.test(userPass)) {
-        return { error: "Password must have a number" }
+        return "Password must have a number"
     }
 
     return false
